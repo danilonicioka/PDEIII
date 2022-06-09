4dd144812301082a2b5ecec54bc692b10985e825
Pedro Rendeiro <76977246+pedro-rendeiro@users.noreply.github.com>
2022-06-09 11:44:16 -0300

    Update test_sensors.c

--- test_sensors/main/test_sensors.c
+++ test_sensors/main/test_sensors.c
@@ -1,16 +1,18 @@
-/* ADC1 Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include <stdlib.h>
+
+///
+#include <string.h>
+///
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
+
+///
+#include <esp_log.h>
+///
+
 #include "rom/ets_sys.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
@@ -20,9 +22,22 @@
 
 #include "DHT22.h"
 
+///
+#include <esp_rmaker_core.h>
+#include <esp_rmaker_standard_types.h>
+
+#include <app_wifi.h>
+#include <app_insights.h>
+
+#include "app_priv.h"
+///
+
 #define DEFAULT_VREF    1093        //Use adc2_vref_to_gpio() to obtain a better estimate
 #define NO_OF_SAMPLES   64          //Multisampling
 
+///
+static const char *TAG = "app_main";
+///
 
 static esp_adc_cal_characteristics_t *adc_chars;
 #if CONFIG_IDF_TARGET_ESP32
@@ -35,6 +50,20 @@ static const adc_bits_width_t width = ADC_WIDTH_BIT_13;
 static const adc_atten_t atten = ADC_ATTEN_DB_11;
 static const adc_unit_t unit = ADC_UNIT_1;
 
+///
+/* Callback to handle commands received from the RainMaker cloud */
+static esp_err_t write_cb(const esp_rmaker_device_t *device, const esp_rmaker_param_t *param,
+            const esp_rmaker_param_val_t val, void *priv_data, esp_rmaker_write_ctx_t *ctx)
+{
+    if (ctx) {
+        ESP_LOGI(TAG, "Received write request via : %s", esp_rmaker_device_cb_src_to_str(ctx->src));
+    }
+    if (app_driver_set_gpio(esp_rmaker_param_get_name(param), val.val.b) == ESP_OK) {
+        esp_rmaker_param_update_and_report(param, val);
+    }
+    return ESP_OK;
+}
+///
 
 void DHT_task(void *pvParameter)
 {
@@ -119,6 +148,66 @@ void app_main(void)
 	vTaskDelay( 1000 / portTICK_RATE_MS );
 	xTaskCreate( &DHT_task, "DHT_task", 2048, NULL, 5, NULL );
 
+    ///============
+    /* Initialize Application specific hardware drivers and
+     * set initial state.
+     */
+    app_driver_init();
+
+    /* Initialize NVS. */
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( err );
+
+    /* Initialize Wi-Fi. Note that, this should be called before esp_rmaker_node_init()
+     */
+    app_wifi_init();
+    
+    /* Initialize the ESP RainMaker Agent.
+     * Note that this should be called after app_wifi_init() but before app_wifi_start()
+     * */
+    esp_rmaker_config_t rainmaker_cfg = {
+        .enable_time_sync = false,
+    };
+    esp_rmaker_node_t *node = esp_rmaker_node_init(&rainmaker_cfg, "ESP RainMaker Device", "GPIO-Device");
+    if (!node) {
+        ESP_LOGE(TAG, "Could not initialise node. Aborting!!!");
+        vTaskDelay(5000/portTICK_PERIOD_MS);
+        abort();
+    }
+
+    /* Create a device and add the relevant parameters to it */
+    esp_rmaker_device_t *firewall_device = esp_rmaker_device_create("Firewall-Device", NULL, NULL);
+    esp_rmaker_device_add_cb(firewall_device, write_cb, read_cb);
+
+    esp_rmaker_param_t *status_param = esp_rmaker_param_create("Status", NULL, esp_rmaker_str("Analise"), PROP_FLAG_READ | PROP_FLAG_WRITE);
+    esp_rmaker_param_add_ui_type(status_param, ESP_RMAKER_UI_TEXT);
+    esp_rmaker_device_add_param(firewall_device, status_param);
+
+    esp_rmaker_node_add_device(node, firewall_device);
+
+    /* Enable Insights. Requires CONFIG_ESP_INSIGHTS_ENABLED=y */
+    app_insights_enable();
+
+    /* Start the Wi-Fi.
+     * If the node is provisioned, it will start connection attempts,
+     * else, it will start Wi-Fi provisioning. The function will return
+     * after a connection has been successfully established
+     */
+    err = app_wifi_start(POP_TYPE_RANDOM);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Could not start Wifi. Aborting!!!");
+        vTaskDelay(5000/portTICK_PERIOD_MS);
+        abort();
+    }
+
+    /* Start the ESP RainMaker Agent */
+    esp_rmaker_start();
+    ///============
+
     //Continuously sample ADC1
     while (1) {
         uint32_t adc_reading = 0;
@@ -137,5 +226,32 @@ void app_main(void)
         uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
         printf("=== Reading MQ6 ===\nRaw: %d\tVoltage: %dmV\n", adc_reading, voltage);
         vTaskDelay(pdMS_TO_TICKS(2000));
+
+        ///===========
+        uint8_t aux1 = 1;
+
+        ///Colocar valor lido em um intervalo
+          ///Mudar valor de aux1 de acordo com o intervalo
+
+        switch (aux1)
+        {
+        case 1:
+            esp_rmaker_param_update_and_report(status_param, "Seguro");
+            break;
+        case 2:
+            esp_rmaker_param_update_and_report(status_param, "Cuidado");
+            esp_rmaker_raise_alert("Verifique seu botijão de gas");
+            break;
+        case 3:
+            esp_rmaker_param_update_and_report(status_param, "Perigo");
+            esp_rmaker_raise_alert("Ligue para os bombeiros");
+            break;
+        default:
+            esp_rmaker_param_update_and_report(status_param, "Analise");
+            break;
+            
+        }
+
+        ///=================
     }
 }
